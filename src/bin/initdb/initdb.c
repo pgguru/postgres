aa17c228aeda5a7f11b38e87d3b7eafab423c3cd
David Christensen <david@pgguru.net>
2023-05-09 16:56:17 -0500

    WIP: selectable encryption methods

--- src/bin/initdb/initdb.c
+++ src/bin/initdb/initdb.c
@@ -3386,7 +3386,6 @@ main(int argc, char *argv[])
 				break;
 			case 19:
 				cluster_key_cmd = pg_strdup(optarg);
-				using_page_feats = true;
 				break;
 			case 'u':
 				old_key_datadir = pg_strdup(optarg);
@@ -3508,6 +3507,9 @@ main(int argc, char *argv[])
 	if (file_encryption_method == DISABLED_ENCRYPTION_METHOD &&
 		cluster_key_cmd != NULL)
 		file_encryption_method = DEFAULT_ENABLED_ENCRYPTION_METHOD;
+
+	/* update page features requirement if encryption method uses authtag */
+	using_page_feats |= (encryption_methods[file_encryption_method].authtag_len > 0);
 
 	check_authmethod_unspecified(&authmethodlocal);
 	check_authmethod_unspecified(&authmethodhost);
